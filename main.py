7e8d913d43af35ae67e1ac9b0a400ca231c6b106
Arnav <ArnavVerma@my.unt.edu>
2025-04-13 23:49:06 -0500

    initiated elevenLabs

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import requests
-from langchain import llms
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+import os 
+load_dotenv()
 
+client = ElevenLabs(
+  api_key=os.getenv("ELEVENLABS_API_KEY"),
+)
 
+audio = client.text_to_speech.convert(
+    text="The first move is what sets everything in motion.",
+    voice_id="JBFqnCBsd6RMkjVDRZzb",
+    model_id="eleven_multilingual_v2",
+    output_format="mp3_44100_128",
+)
+
+play(audio)
